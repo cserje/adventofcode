49ac01542ad67a57a9fe44527b447860a437fb1b
Cserjési Kristóf <cserjesi.kristof@trium-itech.hu>
2023-12-03 19:30:03 +0100

    finish part two

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 let sum = 0;
+let sumOfPowers = 0;
 const [redMax, greenMax, blueMax] = [12, 13, 14];
 
 fs.readFileSync("input.txt", { encoding: "utf-8" })
@@ -8,8 +9,8 @@ fs.readFileSync("input.txt", { encoding: "utf-8" })
   .forEach((row) => {
     const [_, gameId, sets] = row.match(/^Game (\d+):(.*)/);
     let isImpossible = false;
+    let [redMin, greenMin, blueMin] = [0, 0, 0];
     sets.split(";").forEach((set) => {
-      if (isImpossible) return;
       const red = set.match(/(\d+) red/);
       const green = set.match(/(\d+) green/);
       const blue = set.match(/(\d+) blue/);
@@ -19,11 +20,20 @@ fs.readFileSync("input.txt", { encoding: "utf-8" })
       if (redCount > redMax || greenCount > greenMax || blueCount > blueMax) {
         isImpossible = true;
       }
+      redMin = Math.max(redMin, redCount);
+      greenMin = Math.max(greenMin, greenCount);
+      blueMin = Math.max(blueMin, blueCount);
     });
     if (!isImpossible) {
       sum += Number(gameId);
     }
+    sumOfPowers += redMin * greenMin * blueMin;
   });
 
 console.log("--- Part One ---");
 console.log(`The sum of the IDs of those game is: \x1b[32m${sum}\x1b[0m`);
+console.log("");
+console.log("--- Part Two ---");
+console.log(
+  `The sum of the power of these sets is: \x1b[32m${sumOfPowers}\x1b[0m`
+);
