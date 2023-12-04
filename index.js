cd5ae975da47e7b5f4a9fc1cc8ab4d3beba4c9c5
Cserjési Kristóf <cserjesi.kristof@trium-itech.hu>
2023-12-04 11:56:42 +0100

    finish second part

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 
-let sum = 0;
+let lastCardNumber = 0;
+const matchesByCardNumber = new Map();
+const sumOfCardsByCardNumber = new Map();
 const input = fs.readFileSync("input.txt", { encoding: "utf-8" });
 
 input.split("\n").forEach((row) => {
   const h1 = row.split(":");
+  const cardNumber = Number(h1[0].match(/\d+/)[0]);
+  sumOfCardsByCardNumber.set(cardNumber, 1);
   const h2 = h1[1].split("|");
   const matches = new Set();
   const winningNumbers = h2[0].match(/(\d+)/g);
@@ -17,11 +21,31 @@ input.split("\n").forEach((row) => {
       matches.add(value);
     }
   });
-  if (matches.size === 0) {
-    return;
+  matchesByCardNumber.set(cardNumber, matches.size);
+  if (lastCardNumber < cardNumber) {
+    lastCardNumber = cardNumber;
   }
-  sum += Math.pow(2, matches.size - 1);
 });
 
-console.log("--- Part One ---");
-console.log(`How many points are they worth in total?\n\x1b[32m${sum}\x1b[0m`);
+for (let cardNumber = 1; cardNumber <= lastCardNumber; ++cardNumber) {
+  const matchesCount = matchesByCardNumber.get(cardNumber);
+  for (let j = 0; j < sumOfCardsByCardNumber.get(cardNumber); ++j) {
+    for (
+      let i = cardNumber + 1;
+      i <= cardNumber + matchesCount && i <= lastCardNumber;
+      ++i
+    ) {
+      sumOfCardsByCardNumber.set(i, sumOfCardsByCardNumber.get(i) + 1);
+    }
+  }
+}
+
+const sum = Array.from(sumOfCardsByCardNumber.values()).reduce(
+  (a, b) => a + b,
+  0
+);
+
+console.log("--- Part Two ---");
+console.log(
+  `How many total scratchcards do you end up with?\n\x1b[32m${sum}\x1b[0m`
+);
