b4891d2c42c26bfca06a1aa695ee704998fbba49
Cserjési Kristóf <cserjesi.kristof@trium-itech.hu>
2023-12-04 10:11:25 +0100

    finish part two

--- index.js
+++ index.js
@@ -4,12 +4,37 @@ let sum = 0;
 
 const input = fs.readFileSync("input.txt", { encoding: "utf-8" });
 
-input.split("\r\n").forEach((row) => {
-  const matches = row.match(/(\d)/g);
-  if (matches.length === 1) {
-    matches.push(matches[0]);
+const map = new Map();
+map.set("one", 1);
+map.set("two", 2);
+map.set("three", 3);
+map.set("four", 4);
+map.set("five", 5);
+map.set("six", 6);
+map.set("seven", 7);
+map.set("eight", 8);
+map.set("nine", 9);
+
+input.split("\n").forEach((row) => {
+  let digits = [];
+  let sliceIndex = 0;
+  let matches = [];
+  while (
+    (matches = row
+      .slice(sliceIndex)
+      .match(/(one|two|three|four|five|six|seven|eight|nine|\d)/)) !== null
+  ) {
+    digits.push(map.has(matches[0]) ? map.get(matches[0]) : Number(matches[0]));
+    sliceIndex += matches.index + 1;
   }
-  sum += Number(`${matches.slice(0, 1)}${matches.slice(-1)}`);
+
+  if (digits.length === 1) {
+    digits.push(digits[0]);
+  }
+  sum += Number(`${digits.slice(0, 1)}${digits.slice(-1)}`);
 });
 
-console.log(`The sum is: \x1b[32m${sum}\x1b[0m`);
+console.log("--- Part Two ---");
+console.log(
+  `The sum of all of the calibration values is: \x1b[32m${sum}\x1b[0m`
+);
